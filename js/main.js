c40bba18d7913471e2c2c8a2d5b61b2fe4d2d91d
First-Capital-Web-Design <brenthildy@Brents-MacBook-Pro.local>
2016-06-23 15:13:25 -0400

    edits

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 ;(function () {
-	
+
 	'use strict';
 
 
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -91,7 +91,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -110,9 +110,9 @@
 							el.removeClass('item-animate');
 						},  k * 200, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -121,12 +121,12 @@
 	var parallax = function() {
 		$(window).stellar({
 			horizontalScrolling: false,
-			hideDistantElements: false, 
+			hideDistantElements: false,
 			responsive: true
 
 		});
 	};
-	
+
 
 
 	// Document on load.
